c8640d822d6abd22d7885140513365c66f45e219
Arron <beale86@msn.com>
2022-07-05 19:24:40 +0000

    Add github-information.js, add function fetchGitHubInformation

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -6,4 +6,18 @@ function initMap() {
             lng: -6.237322
         }
     });
+
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var locations = [
+        {lat: 53.342899, lng:-6.237089}
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
